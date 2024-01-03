02b75be0b74c4c51c15636d10c0b2cfbeadafebc
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-01-03 11:51:36 +0530

    first commit56

--- src/Components/Hooks/UserDetails.js
+++ src/Components/Hooks/UserDetails.js
@@ -9,14 +9,14 @@ function UserDetailsHooks(UserName) {
   const [isloading, setisloading] = useState(true); // Indicates whether data is being fetched.
 
   // Function to fetch user data from the GitHub API based on the provided UserName or default query term.
-  const getData = async () => {
+    async function DownloadData(){
     setisloading(true); // Set loading state to true while fetching data.
 
     if (UserName) {
       try {
         // Fetch data based on the provided UserName parameter.
-        const resp = await fetch(`https://api.github.com/search/users?q=${UserName}`);
-        const data = await resp.json();
+        const response = await fetch(`https://api.github.com/search/users?q=${UserName}`);
+        const data = await response.json();
         setphotoData(data.items); // Update UserDetails state with fetched user data.
       } catch (error) {
         console.log("Error while fetching Data", error.message);
@@ -24,19 +24,19 @@ function UserDetailsHooks(UserName) {
     } else {
       try {
         // Fetch data based on the default query term if UserName is not provided.
-        const resp = await fetch(`https://api.github.com/search/users?q=${query || "Farman"}`);
-        const data = await resp.json();
+        const response = await fetch(`https://api.github.com/search/users?q=${query || "Farman"}`);
+        const data = await response.json();
         setphotoData(data.items); // Update UserDetails state with fetched user data.
       } catch (error) {
         console.log("Error while fetching Data", error.message);
       }
     }
     setisloading(false); // Set loading state to false after fetching data.
-  };
+  }
 
   // useEffect hook to perform side effects in the functional component.
   useEffect(() => {
-    getData(); // Call the getData function when the component mounts or when the query state changes.
+    DownloadData(); // Call the getData function when the component mounts or when the query state changes.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]); // Re-run the effect if the 'query' state changes.
 
